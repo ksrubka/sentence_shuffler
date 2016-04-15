95eba12412c7a020230a7b9b0efe6e90c49bc364
ksrubka <beata.choluj@wp.pl>
2016-04-15 18:34:48 +0200

    Create new test

--- src/SentenceShufflerTestApp.java
+++ src/SentenceShufflerTestApp.java
@@ -1,11 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Beata Iłowiecka on 14.04.16.
  */
 public class SentenceShufflerTestApp {
 
     public static void main(String[] args) {
-        SentenceShuffler zdanie1 = new SentenceShuffler("Oczywiście, że uwielbiam programować, bo niby po co ja sobie tutaj żyły wypruwam, bo lubię?!");
 
-        System.out.println(zdanie1.getShuffledSentence());
+        SentenceShuffler zdanie1 = new SentenceShuffler("When You feel the need to write a comment, " +
+                "first try to refactor the code so that any comment becomes superfluous");
+
+        SentenceShuffler zdanie2 = new SentenceShuffler("There is no reasonable excuse " +
+                "for doing anything less than Your best");
+
+        SentenceShuffler zdanie3 = new SentenceShuffler("Tworzenie struktury kodu pod kątem łatwości " +
+                "testowania prowadzi do otrzymania lepszego projektu kodu.");
+
+        System.out.println(zdanie1.getShuffledSentence()+ "\n");
+        System.out.println(zdanie2.getShuffledSentence() + "\n");
+        System.out.println(zdanie3.getShuffledSentence());
     }
 }
